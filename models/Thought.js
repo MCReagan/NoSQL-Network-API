9f6631b5496bfa38d4c91f6527610ad487188e80
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-29 16:10:28 -0500

    since reactions only live on thoughts, I thought it may be a good idea to use a single schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => timestamp.toLocaleString()
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -17,10 +45,7 @@ const thoughtSchema = new Schema(
             type: String,
             required: true
         },
-        reactions: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Reaction'
-        }]
+        reactions: [reactionSchema],
     },
     {
       toJSON: {
